ad9522125c861fe447f18c5e46202b67416df3b8
syedmehedi0003 <mehedihasanbahar@gmail.com>
2026-06-27 11:43:42 +0600

    sussecc test

--- test_calculator.py
+++ test_calculator.py
@@ -2,7 +2,7 @@ import pytest
 from calculator import add, subtract, multiply, divide
 
 def test_add():
-    assert add(2, 3) == 7
+    assert add(2, 3) == 5
     assert add(-1, 1) == 0
     assert add(0, 0) == 0
 
